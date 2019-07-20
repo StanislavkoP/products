619507e7aebe40554c19f2745f036835468b1f1c
StanislavkoP <stas8327634@gmail.com>
2019-07-20 13:24:51 +0300

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { useRoutes, navigate } from 'hookrouter';
+import { useStateValue } from './state/StateContext';
+import { getItemFromLocalStorage } from './Share/LocalStorage';
+
+import Header from './Components/Header/Header';
+import AuthPage from './Container/Auth/Auth';
+import LogOutPage from './Container/LogOut';
+import DashboardPage from './Container/Dashboard';
+import ProductPage from './Container/Product/Product';
+
 import './App.css';
 
+
+const routes = {
+  '/': () => <DashboardPage/>,
+  '/auth': () => <AuthPage/>,
+  '/logout': () => <LogOutPage />,
+  '/dashboard': () => <DashboardPage/>,
+  '/product/:id': ({id}) => <ProductPage productId={id} />,
+};
+
 function App() {
+  const routeResult = useRoutes(routes);
+  const [, dispatch] = useStateValue();
+
+  useEffect(() => {
+    if (getItemFromLocalStorage('token')) {
+      const userName = getItemFromLocalStorage('userName');
+
+      dispatch({
+          type: 'AUTH_USER_SUCCESS',
+          isLoggedIn: true,
+          userName
+      });
+
+      setTimeout(() => {
+        dispatch({type: 'USER_LOG_OUT'});
+        navigate('/logout', true);
+        
+      }, 120 * 60 * 1000)
+
+      navigate('/dashboard', true);
+
+    }
+  }, [dispatch])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Header/>
+
+        { routeResult }
+      </div>
   );
 }
 
