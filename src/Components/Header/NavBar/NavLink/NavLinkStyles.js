46f7ea9eef1a5a8b511adc289f0509f33c57376b
StanislavkoP <stas8327634@gmail.com>
2019-07-21 22:06:23 +0300

    Changed style

--- src/Components/Header/NavBar/NavLink/NavLinkStyles.js
+++ src/Components/Header/NavBar/NavLink/NavLinkStyles.js
@@ -2,5 +2,47 @@ import { A } from 'hookrouter';
 import styled from 'styled-components';
 
 export const StyledNavLink = styled(A)`
+    position: relative;
+    display: inline-block;
+    padding: 12px;
+    outline: none;
+    color: black;
+    text-decoration: none;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: 400;
+    text-shadow: 0 0 1px rgba(255,255,255,0.3);
 
+    &::before {
+        position: absolute;
+        top: 100%;
+        left: 0;
+        width: 100%;
+        height: 1px;
+        background: black;
+        content: '';
+        opacity: 0;
+        transition: height 0.3s, opacity 0.3s, transform 0.3s;
+        transform: translateY(-10px);
+
+    }
+    
+    &:hover {
+        &::before {
+            height: 3px;
+            opacity: 1;
+            transform: translateY(0px);
+            
+        }
+    }
+
+    &.active {
+        cursor: default;
+        &::before {
+            height: 3px;
+            opacity: 1;
+            transform: translateY(0px);
+            
+        }
+    }
 `;
