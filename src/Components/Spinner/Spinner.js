adbe05ca3d931b98f457c3e01da48d1cfd2ebc8f
StanislavkoP <stas8327634@gmail.com>
2019-07-24 19:42:04 +0300

    Added spinner

---
+++ src/Components/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyledSpinner } from './SpinnerStyles';
+
+
+function Spinner() {
+    return (
+        <StyledSpinner>Loading...</StyledSpinner>    
+    )
+};
+
+export default Spinner;
